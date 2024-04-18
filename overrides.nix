51f5b389db83971bc86cf9398b112fc2ad82c769
dnaq <dnaq@users.noreply.github.com>
2024-04-18 06:28:10 +0200

    plover-controller: don't run tests
    The plugin seemed to reference files in its
    test suite that were part of its source code
    but weren't part of the built package. This
    works around that issue.

--- overrides.nix
+++ overrides.nix
@@ -1,4 +1,4 @@
-{ ruamel-yaml, prompt-toolkit, pysdl2, setuptools-scm }:
+{ ruamel-yaml, prompt-toolkit, pysdl2, setuptools-scm, utils }:
 self: super: {
   plover_yaml_dictionary = super.plover_yaml_dictionary.overrideAttrs (old: {
     propagatedBuildInputs = [ ruamel-yaml ];
@@ -10,6 +10,8 @@ self: super: {
   });
   plover-controller = super.plover-controller.overrideAttrs (old: {
     propagatedBuildInputs = [ pysdl2 ];
+    doCheck = false;
+    doInstallCheck = false;
   });
   plover_dict_commands = super.plover_dict_commands.overrideAttrs (old: {
     propagatedBuildInputs = [ setuptools-scm ];
