e6190b3ae5a476282806b19030e3cb73d7a65069
LilleAila <olai.solsvik@gmail.com>
2025-04-13 22:07:09 +0200

    plugins: remove old rename warning

--- plugins.nix
+++ plugins.nix
@@ -8,38 +8,25 @@
 }:
 let
   inherit (lib) extends;
-  getName = plugin: lib.lists.head (builtins.split "-[0-9]" plugin.filename);
   plugins = builtins.fromJSON (builtins.readFile ./plugins.json);
   makePloverPlugin =
     plugin:
     (buildPythonPackage rec {
       pname = plugin.name;
-      version = plugin.version;
+      inherit (plugin) version;
       src = fetchPypi {
         inherit version;
         pname = lib.lists.head (builtins.split "-[0-9]" plugin.filename);
-        sha256 = plugin.sha256;
+        inherit (plugin) sha256;
       };
       buildInputs = [ plover ];
     });
-  makeRenamedPloverPlugin =
-    plugin:
-    let
-      name = getName plugin;
-    in
-    {
-      inherit name;
-      value = throw "The plugin ${name} has been renamed to ${plugin.name}. See the readme for more info.";
-    };
   pluginToAttr = p: {
     name = p.pname;
     value = p;
   };
   basicPlugins =
-    final: prev:
-    builtins.listToAttrs (map (p: makeRenamedPloverPlugin p) plugins)
-    # Concat the sets and make sure the correct names have priority over the renamed ones
-    // builtins.listToAttrs (map (p: pluginToAttr (makePloverPlugin p)) plugins);
+    final: prev: builtins.listToAttrs (map (p: pluginToAttr (makePloverPlugin p)) plugins);
   overrides = callPackage ./overrides.nix { };
 
   initialPackages = self: callPackage ./extra-plugins.nix { inherit plover inputs; };
